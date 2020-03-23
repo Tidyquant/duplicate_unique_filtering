6af55afbbba8535a54097cec42d8ac4a99d8cdee
Yana Hontarenko <y.hontarenko@spd-ukraine.com>
2020-03-23 09:49:11 +0200

    create main script

---
+++ main.py
@@ -0,0 +1,16 @@
+import argparse
+
+from duplicate_remover import DuplicateRemover
+
+# Parser of command line arguments
+parser = argparse.ArgumentParser(description='Script for find duplicate in data using Levenshtein distance')
+parser.add_argument('data', type=str, required=True, help='Path to the file with data')
+parser.add_argument('threshold', type=int, default=2,
+                    help='Maximum value for returned Levenshtein distance between two samples in data')
+parser.add_argument('folder-to-save', type=str, required=True, help='Path(folder) to save new data')
+
+# Parse arguments of command line
+args = parser.parse_args()
+
+duplicate_remover = DuplicateRemover(args.data, args.path_to_save, args.threshold)
+duplicate_remover.process()
